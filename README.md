766ec862bde5f1f146ef9a64a53b18b7b9f48bb2
Abhi53003 <abhiweb29@gmail.com>
2024-07-30 07:44:44 +0000

    chnage a lot

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # gsap_examples
+___
+### TWEEN
+tween is use for all animation work 
+**there are three animation properties**
+
+* gsap.to()
+* gsap.from()
+* gsap.tofrom()
+---
+**Animation triggring options**
+
+1) this option is use when you want to trigger amimation of an element using same element or any other element 
+
+like we have two box at different position box1>top:200px
+and box2>top:400px,
+so using animationtriger we can animate box1 when box2 comes underview port, simple means box1 is dependent on box1
+---
+**we can pass animation trigger as object and other properties as well**
+
+```
+scrollTrigger:
+{
+    trigger:".square",
+    start:
+    end:
+}
+```
+the above snippet showing scroll trigger with two additional property start and end 
+
+**Start**
+this propert tells when to start the animation or property
+* we can pass Numbers e.g. :- `start:500`
+it means the trigger will start when element move 500 px after entering in viewport.
+
+* we can also pass string as `start:"top center"
+this means animation starts when top of element center of scroll bar
+
+**marker**
+these are top bottom and scroll labels
--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// to add plugin in our website we can use register plugin 
+
+
 gsap.from('.main',
     {
         duration:1,
@@ -22,4 +25,55 @@ gsap.from('.text',
             x:"-100%",
             opacity:1,
         })
+
+
+
+gsap.to(".img_12",{
+    opacity:0.5
+    })
+
+
+    gsap.from('.box', {
+        // "scroll trigger is value given to the element we want to trigger"
+        // for example we want to animate .box when box2 enter in view port
+        //BOX WILL START ANIMAING WHEN BOX2 COMES IN VIEW PORT
+        scrollTrigger: '.box2', 
+      
+        opacity:0,
+       
+    });
     
+
+    gsap.to('.box', {
+        // "scroll trigger is value given to the element we want to trigger"
+        // for example we want to animate .box when box2 enter in view port
+        //BOX WILL START ANIMAING WHEN BOX2 COMES IN VIEW PORT
+        scrollTrigger: '.box2', 
+        width:'500px',
+        opacity:1,
+        borderRadius:0,
+        x:'-200px' ,
+    });
+
+    gsap.from('.box3', {
+        // "scroll trigger is value given to the element we want to trigger"
+        // for example we want to animate .box when box2 enter in view port
+        //BOX WILL START ANIMAING WHEN BOX2 COMES IN VIEW PORT
+        scrollTrigger: '.box2', 
+      
+        opacity:0,
+       
+    });
+    
+
+    gsap.to('.box3', {
+        // "scroll trigger is value given to the element we want to trigger"
+        // for example we want to animate .box when box2 enter in view port
+        //BOX WILL START ANIMAING WHEN BOX2 COMES IN VIEW PORT
+        scrollTrigger: '.box4', 
+        width:'500px',
+        opacity:1,
+
+        borderRadius:0,
+        x:'200px' ,
+    });
